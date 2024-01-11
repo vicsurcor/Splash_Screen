b2e671fe4b0725df299366f9ece473c7f11212a2
vicsurcor <vdsurcor@gmail.com>
2024-01-11 18:37:02 +0100

    Fin

---
+++ app/src/main/java/com/example/splash_screenvictorsuros/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.splash_screenvictorsuros
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
